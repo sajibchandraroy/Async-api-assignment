f92f36a5e242aa3915361a9ec671d654c99eec7f
Sumita5 <satyakiroyhreeg@gmail.com>
2020-12-19 13:14:36 +0600

    some change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import ApiPoller from './Home/ApiPoller';
+import Home from './Home/Home';
+
 
 
 
@@ -8,6 +10,7 @@ function App() {
   return (
     <div className="App">
     <ApiPoller/>
+    <Home/>
     </div>
   );
 }
--- src/Home/Post.js
+++ src/Home/Post.js
@@ -17,7 +17,7 @@ const Post = ({data}) => {
 
     return (
        <div>
-           <Home />
+           
        </div>
     );
 };
